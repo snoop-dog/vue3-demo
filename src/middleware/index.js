d525f6c3b7089e06862568556a607b4074484bcc
qilinjie <qilinjie1991@outlook.com>
2020-09-07 10:17:26 +0800

    update: axios cancel request

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -7,6 +7,17 @@ const request = axios.create({
   // withCredentials: true,
 })
 
+let pending = [] // 声明一个数组用于存储每个ajax请求的取消函数和ajax标识
+const cancelToken = axios.CancelToken
+const removePending = (ever) => {
+  for(let p in pending){
+    if(pending[p].u === ever.url + '&' + ever.method) { // 当当前请求在数组中存在时执行函数体
+      pending[p].f() // 执行取消操作
+      pending.splice(p, 1) // 把这条记录从数组中移除
+    }
+  }
+}
+
 request.interceptors.request.use(
   (config) => {
     if (config.method.toUpperCase() == 'GET') {
@@ -15,6 +26,12 @@ request.interceptors.request.use(
       config.headers['content-type'] = 'appliaction/x-www-form-urlencoded'
       config.data = qs.stringify(config.data)
     }
+
+    removePending(config) // 在一个ajax发送前执行一下取消操作
+    config.cancelToken = new cancelToken((c) => {
+        pending.push({ u: config.url + '&' + config.method, f: c })
+    })
+    
     return config
   },
   (err) => {
@@ -24,6 +41,7 @@ request.interceptors.request.use(
 
 request.interceptors.response.use(
   (res) => {
+    removePending(res.config)
     if (res.statusText == 'OK') {
       return res.data
     }
